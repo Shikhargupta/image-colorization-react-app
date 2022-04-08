43a56b8b956a7d7e0480a2baef208e49cc68b8a3
Shikhar Gupta <shikhar@Shikhars-MacBook-Air.local>
2022-04-08 13:43:34 -0400

    drop down

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
 import React from 'react';
 
+// global variables to change where necessary
+const DROPDOWN_API_ENDPOINT = 'https://1g12qzycb4.execute-api.us-east-2.amazonaws.com/prod/';
+const ML_API_ENDPOINT = 'https://ap6lik2enl.execute-api.us-east-2.amazonaws.com/prod/';
+
+
 // atob is deprecated but this function converts base64string to text string
 const decodeFileBase64 = (base64String) => {
   // From Bytestream to Percent-encoding to Original string
@@ -17,7 +22,32 @@ function App() {
   const [inputFileData, setInputFileData] = React.useState(''); // represented as bytes data (string)
   const [outputFileData, setOutputFileData] = React.useState(''); // represented as readable data (text string)
   const [buttonDisable, setButtonDisable] = React.useState(true);
+  const [inputImage, setInputImage] = React.useState('');
   const [buttonText, setButtonText] = React.useState('Submit');
+  const [submitButtonText, setSubmitButtonText] = React.useState('Submit');
+  const [fileButtonText, setFileButtonText] = React.useState('Upload File');
+  const [demoDropdownFiles, setDemoDropdownFiles] = React.useState([]);
+  const [selectedDropdownFile, setSelectedDropdownFile] = React.useState('');
+
+
+  // make GET request to get demo files on load -- takes a second to load
+  React.useEffect(() => {
+    fetch(DROPDOWN_API_ENDPOINT)
+    .then(response => response.json())
+    .then(data => {
+      // GET request error
+      if (data.statusCode === 400) {
+        console.log('Sorry! There was an error, the demo files are currently unavailable.')
+      }
+
+      // GET request success
+      else {
+        const s3BucketFiles = JSON.parse(data.body);
+        setDemoDropdownFiles(s3BucketFiles["s3Files"]);
+      }
+    });
+  }, [])
+
 
   // convert file to bytes data
   const convertFileToBytes = (inputFile) => {
@@ -66,7 +96,7 @@ function App() {
 
     // make POST request
     console.log('making POST request...');
-    fetch('https://ap6lik2enl.execute-api.us-east-2.amazonaws.com/prod/', {
+    fetch(ML_API_ENDPOINT, {
       method: 'POST',
       headers: { "Content-Type": "application/json", "Accept": "text/plain" },
       body: JSON.stringify({ "image": inputFileData })
@@ -96,17 +126,61 @@ function App() {
     })
   }
 
+  // handle demo dropdown file selection
+  const handleDropdown = (event) => {
+    setSelectedDropdownFile(event.target.value);
+
+    // temporarily disable submit button
+    setButtonDisable(true);
+    setSubmitButtonText('Loading Demo File...');
+
+    // only make POST request on file selection
+    if (event.target.value) {
+      fetch(DROPDOWN_API_ENDPOINT, {
+        method: 'POST',
+        body: JSON.stringify({ "fileName": event.target.value })
+      }).then(response => response.json())
+      .then(data => {
+
+        // POST request error
+        if (data.statusCode === 400) {
+          console.log('Uh oh! There was an error retrieving the dropdown file from the S3 bucket.')
+        }
+
+        // POST request success
+        else {
+          const dropdownFileBytesData = JSON.parse(data.body)['bytesData'];
+          setInputFileData(dropdownFileBytesData);
+          setInputImage('data:image/png;base64,' + dropdownFileBytesData); // hacky way of setting image from bytes data - even works on .jpeg lol
+          setSubmitButtonText('Submit');
+          setButtonDisable(false);
+        }
+      });
+    }
+
+    else {
+      setInputFileData('');
+    }
+  }
+
+
   return (
     <div className="App">
       <div className="Input">
-        <h1>Input</h1>
+        <h1>Input Image</h1>
+        <label htmlFor="demo-dropdown">Demo: </label>
+        <select name="Select Image" id="demo-dropdown" value={selectedDropdownFile} onChange={handleDropdown}>
+            <option value="">-- Select Demo File --</option>
+            {demoDropdownFiles.map((file) => <option key={file} value={file}>{file}</option>)}
+        </select>
         <form onSubmit={handleSubmit}>
+          {/* <img src={inputFileData} alt="input image" /> */}
           <input type="file" accept=".png" onChange={handleChange} />
           <button type="submit" disabled={buttonDisable}>{buttonText}</button>
         </form>
       </div>
       <div className="Output">
-        <h1>Results</h1>
+        <h1>Result</h1>
         <img src={outputFileData} alt="" />
       </div>
     </div>
